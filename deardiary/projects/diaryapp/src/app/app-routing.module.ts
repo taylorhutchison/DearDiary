3e029ffb32224b46ac52019d0cc54a96709cd6ce
Taylor Hutchison <seanthutchison@gmail.com>
2021-04-08 13:06:44 -0500

    Add shell route

--- deardiary/projects/diaryapp/src/app/app-routing.module.ts
+++ deardiary/projects/diaryapp/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppShellComponent } from './app-shell/app-shell.component';
 
 const routes: Routes = [
+  { path: 'shell', component: AppShellComponent },
   { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
   { path: 'new', loadChildren: () => import('./modules/newentries/newentries.module').then(m => m.NewentriesModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
