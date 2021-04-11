be43aa2e0f7b7026c581682c6bed20f2ee1a1560
Taylor Hutchison <seanthutchison@gmail.com>
2021-04-11 16:21:34 -0500

    Added pwa update.

--- deardiary/projects/diaryapp/src/app/app.component.ts
+++ deardiary/projects/diaryapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { NetworkService } from './services/network.service';
 import { NotificationService } from './services/notification.service';
 import { StorageService } from './services/storage.service';
@@ -11,7 +12,11 @@ import { StorageService } from './services/storage.service';
 export class AppComponent implements OnInit {
   title = 'diaryapp';
   isOnline: boolean = false;
-  constructor(private store: StorageService, private notificationService: NotificationService, private networkService: NetworkService) {
+  constructor(private store: StorageService,
+    private notificationService: NotificationService,
+    private networkService: NetworkService,
+    private updates: SwUpdate
+  ) {
   }
 
   ngOnInit() {
@@ -22,6 +27,13 @@ export class AppComponent implements OnInit {
       } else {
         this.notificationService.notify('Offline mode enabled');
       }
+    });
+
+    this.updates.available.subscribe(event => {
+      this.notificationService.notify(`An update is available. Current version ${event.current}. Available version ${event.available}.`);
+    });
+    this.updates.activated.subscribe(event => {
+      this.notificationService.notify(`App Updated. Old version was ${event.previous}. Available version ${event.current}.`);
     });
   }
 }
